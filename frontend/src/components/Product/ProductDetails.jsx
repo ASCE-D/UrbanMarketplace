f61968cf0675b979aa7141dc26962f05cb05327b
Tehnoking <ashispandey138cgmail.com>
2023-08-26 17:31:34 +0530

    review system implemented

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { getProductDetails } from "../../actions/productAction";
+import { getProductDetails, newReview } from "../../actions/productAction";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import Loader from "../layouts/Loader/Loader";
+import ReviewCard from "./ReviewCard";
 
 const ProductDetails = () => {
   const params = useParams();
   console.log(params);
   const { product } = useSelector((state) => state.product);
   const dispatch = useDispatch();
+   
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     dispatch(getProductDetails(params.id));
@@ -21,6 +26,22 @@ const ProductDetails = () => {
     dispatch({ type: "calculatePrice" });
     toast.success("Added To Cart");
   };
+  const submitReviewToggle = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+
+  const reviewSubmitHandler = () => {
+    const myForm = new FormData();
+
+    myForm.set("rating", rating);
+    myForm.set("comment", comment);
+    myForm.set("productId", params.id);
+
+    dispatch(newReview(myForm));
+
+    setOpen(false);
+  };
+
   return (
     <>
       <div>
@@ -70,8 +91,71 @@ const ProductDetails = () => {
             >
               Add to Cart
             </button>
+
+            <button
+  onClick={submitReviewToggle}
+  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+>
+  Submit Review
+</button>
           </div>
         </div>
+        <h3 className="text-xl font-semibold mt-4">REVIEWS</h3>
+
+<div
+  className={`${
+    open ? 'block' : 'hidden'
+  } fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 shadow-lg z-50`}
+>
+  <h2 className="text-lg font-semibold mb-2">Submit Review</h2>
+  <div className="submitDialog">
+    <select
+      onChange={(e) => setRating(e.target.value)}
+      value={rating}
+      className="w-full p-2 mb-2 border border-gray-300"
+    >
+      <option value="1">1 Star</option>
+      <option value="2">2 Stars</option>
+      <option value="3">3 Stars</option>
+      <option value="4">4 Stars</option>
+      <option value="5">5 Stars</option>
+    </select>
+
+    <textarea
+      className="submitDialogTextArea w-full p-2 border border-gray-300"
+      cols="30"
+      rows="5"
+      value={comment}
+      onChange={(e) => setComment(e.target.value)}
+    ></textarea>
+
+    <div className="mt-2">
+      <button
+        onClick={submitReviewToggle}
+        className="mr-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Cancel
+      </button>
+      <button
+        onClick={reviewSubmitHandler}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</div>
+
+{product.reviews && product.reviews[0] ? (
+  <div className="reviews">
+    {product.reviews &&
+      product.reviews.map((review) => (
+        <div key={review._id}><ReviewCard key={review._id} review={review}/></div>
+      ))}
+  </div>
+) : (
+  <p className="text-gray-500 mt-4">No Reviews Yet</p>
+)}
       </div>
     </>
   );
