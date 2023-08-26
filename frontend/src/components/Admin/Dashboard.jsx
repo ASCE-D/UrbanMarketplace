a74bdbca4ccf374054118fa06ac7a984d5448b00
Deepesh Genani <deepeshgenani@gmail.com>
2023-08-26 18:21:33 +0530

    added dashboard

--- frontend/src/components/Admin/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -1,48 +1,57 @@
-import React from 'react'
-import { getAdminProduct } from '../../actions/productAction';
-import { getAllUsers } from '../../actions/userAction';
-import { useEffect } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { getAdminProduct } from "../../actions/productAction";
+import { getAllUsers } from "../../actions/userAction";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
 const Dashboard = () => {
   const { user } = useSelector((state) => state.user);
- 
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(getAllUsers(user));
-    }, [dispatch]);
-     const { users } = useSelector((state) => state.allUsers);
-    console.log(users);
-    
-    const navigate = useNavigate();
+  const { users } = useSelector((state) => state.allUsers);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  
+  useEffect(() => {
+    dispatch(getAllUsers(user));
+  }, [dispatch, user]);
+
   return (
-    <div>
-      {user.role}
-      {users[0].email}
- 
-    </div>
-  )
-}
+    <>
+      <div className="min-h-screen bg-gray-100 py-6 lg:ml-56">
+      
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+          <p className="text-gray-700 mt-2">Logged in as: {user.email}</p>
+          <div className="mt-6 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+            {users &&
+              users.map((u) => (
+                <div
+                  key={u._id}
+                  className={`bg-white overflow-hidden shadow-md rounded-lg ${
+                    u._id === user._id ? "outline-dotted" : ""
+                  }`}
+                >
+                  <div className="p-4">
+                    <h3 className="text-lg font-medium text-gray-900">
+                      Name: {u.name}
+                    </h3>
+                    <p className="mt-2 text-sm text-gray-500">
+                      Email: {u.email}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">
+                      CreatedAt: {u.createdAt}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">Role: {u.role}</p>
 
-export default Dashboard
+                    {/* Add more user information here */}
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-    // const { users } = useSelector((state) => state.allUsers);
-
-
-    
-    // const { products } = useSelector((state) => state.products);
-  
-    // const { orders } = useSelector((state) => state.allOrders);
-  
-    
-  //  console.log(users)
-  //   useEffect(() => {
-  //       dispatch(getAdminProduct());
-  //       // dispatch(getAllOrders());
-  //       dispatch(getAllUsers());
-  //     }, [dispatch]);
-  //     const { users } = useSelector((state) => state.allUsers);
-  //     console.log(users)
+export default Dashboard;
