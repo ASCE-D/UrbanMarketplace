ff7c277e952814e601ef8a285ec28f35f3d65b96
Ashish2271 <ashispandey138c@gmail.com>
2023-10-01 03:14:42 +0530

    bug fix chakra ui not working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import {store, persistor} from "./redux/store.js";
+import { ColorModeScript ,ChakraProvider,theme} from '@chakra-ui/react';
 
 import { PersistGate } from "redux-persist/integration/react";
 import Loader from "./components/layouts/Loader/Loader.jsx";
@@ -15,7 +16,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Loader />} persistor={persistor}>
+      <ChakraProvider theme={theme}>
         <App />
+        </ChakraProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
