21819daa329320a0cb27d8f58f9001094fe2ce4d
Ashish Pandey <97328833+Ashish2271@users.noreply.github.com>
2023-10-09 01:03:02 +0530

    Update Sidebar.jsx

--- frontend/src/components/Admin/Sidebar.jsx
+++ frontend/src/components/Admin/Sidebar.jsx
@@ -1,101 +1,103 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
 
 const Sidebar = () => {
-  const {user} = useSelector((state) => 
-    state.user)
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <div>
-      <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
-        type="button"
-        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-black rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    <>
+      <Button
+        zIndex={'overlay'}
+        pos={'fixed'}
+        top={'4'}
+        left={'4'}
+        colorScheme="purple"
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
       >
-        <span className="sr-only">Open sidebar</span>
-        <svg
-          className="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clipRule="evenodd"
-            fillRule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          />
-        </svg>
-      </button>
+        <BiMenuAltLeft size={'20'} />
+      </Button>
 
-      <aside
-        id="default-sidebar"
-        className="h-screen top-0 left-0 w-64 transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 py-4 overflow-y-auto ">
-          <ul className="space-y-2 font-medium my-20 items-center">
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-200 dark:hover:text-gray-700 group"
-              >
-                <svg
-                  className="w-5 h-5 text-gray-800 transition duration-75 dark:text-gray-400 group-hover:text-black dark:group-hover:text-black"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
-                <span className="ml-3">Profile</span>
-              </a>
-            </li>
-            { user.role === "admin" ? <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-200 dark:hover:text-gray-700 group"
-              >
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-black"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 18"
-                >
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                </svg>
-                <span className="ml-3">Admin Dashboard</span>
-              </a>
-            </li> : null}
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-200 dark:hover:text-gray-700 group"
-              >
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-black transition duration-75 dark:text-gray-400 group-hover:text-black dark:group-hover:text-black"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 18"
-                >
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                </svg>
-                <span className="ml-3">Orders</span>
-              </a>
-            </li>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
 
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>UrbanMarketPlace</DrawerHeader>
+          <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/admin/products'}>Products</Link>
+              </Button>
 
-          </ul>
-        </div>
-      </aside>
-    </div>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/admin/users'}>users</Link>
+              </Button>
+
+              {/* <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos?category=free'}>orders</Link>
+              </Button> */}
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/admin/product'}>NewProduct</Link>
+              </Button>
+            </VStack>
+
+            {/* <HStack
+              pos={'absolute'}
+              bottom={'10'}
+              left={'0'}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme={'purple'}>
+                <Link to={'/login'}>Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                colorScheme={'purple'}
+                variant={'outline'}
+              >
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
+            </HStack> */}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
-
-export default Sidebar;
+ 
+export default Sidebar
