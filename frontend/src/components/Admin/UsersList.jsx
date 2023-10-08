71eb9f84691eeda9f0e83aafce77004392ded2ed
Ashish Pandey <97328833+Ashish2271@users.noreply.github.com>
2023-10-09 01:05:06 +0530

    Update UsersList.jsx

--- frontend/src/components/Admin/UsersList.jsx
+++ frontend/src/components/Admin/UsersList.jsx
@@ -19,7 +19,7 @@ const UsersList = () => {
 
   return (
     <>
-    <Dashboard/>
+    <Sidebar/>
       <div className="min-h-screen bg-gray-100 py-6">
       
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
